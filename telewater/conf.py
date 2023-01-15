bd344e0a47105ebe095a51cdd552dd294150f0aa
Jensen <sakurazuka@qq.com>
2023-01-15 16:40:41 +0800

    Update conf.py

--- telewater/conf.py
+++ telewater/conf.py
@@ -9,7 +9,6 @@ class Config(BaseModel):
     frame_rate: int = 15
     preset: str = "ultrafast"
     position: Position = Position.centre
-    curWM : str = "image.png"
 
     @validator("preset")
     def validate_preset(val):
@@ -17,6 +16,7 @@ class Config(BaseModel):
         if not val in allowed:
             raise ValueError(f"Choose preset from {allowed}")
         return val
+
 
 START = """正在工作中!"""
 
@@ -30,20 +30,14 @@ GET语法为： `/get` ➜  `/get key`
 
 """
 
-SETWM1 = """成功设置水印为『西安儒雅群，t.me/xianruya』"""
-SETWM2 = """成功设置水印为『https://t.me/xianruya』"""
-SETWM3 = """成功设置水印为『@xianruya』"""
-SETWM4 = """成功设置水印为『@xahades http://t.me/xaHades』"""
-SETWM5  = """成功设置水印为『@PreHades』"""
-
 COMMANDS = {
     "start": "尝试启动BOT，查看是否在工作中",
     "get": "获取当前配置",
-    "set wm 1" : "西安儒雅群，t.me/xianruya",
-    "set wm 2" : "https://t.me/xianruya",
-    "set wm 3" : "@xianruya",
-    "set wm 4" : "@xahades http://t.me/xaHades",
-    "set wm 5" : "@PreHades",
+    "setWM1" : "西安儒雅群，t.me/xianruya",
+    "setWM2" : "https://t.me/xianruya",
+    "setWM3" : "@xianruya",
+    "setWM4" : "@xahades http://t.me/xaHades",
+    "setWM5" : "@PreHades",
     "set": "通过SET命令设置配置",
     "help": "使用帮助",
 }
