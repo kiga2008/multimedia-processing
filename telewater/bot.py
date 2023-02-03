489f130beb91677d0c84f469f2c00be4cf612978
Jensen <sakurazuka@qq.com>
2023-02-03 14:13:43 +0800

    Update bot.py

--- telewater/bot.py
+++ telewater/bot.py
@@ -190,7 +190,7 @@ def apply_wm(
         "-i",
         f"color=black@0:s=300*300,format=yuva420p",
         "-filter_complex",
-        f"[1]trim=end_frame=1,drawtext=font='Roboto':text={watermark_text}:fontcolor=white:fontsize=24:x=0:y=150:alpha=0.8,rotate=a=30*PI/180:c=black@0,loop=-1:1:0,tile=20x20,trim=end_frame=1[wm];[0][wm]overlay=0:0",
+        f"[1]trim=end_frame=1,drawtext=font='Roboto':text='西安儒雅群 @t.me/xianruya':fontcolor=white:fontsize=24:x=0:y=150:alpha=0.8,rotate=a=30*PI/180:c=black@0,loop=-1:1:0,tile=20x20,trim=end_frame=1[wm];[0][wm]overlay=0:0",
         "-c:a",
         "copy",
         "-preset",
