07b961848c05a693747a22dfd5f5b10b3657af5e
Meowtlen <meowtlen@Meowtlens-Laptop.local>
2023-02-18 06:17:19 +0800

    增加一个水印

--- telewater/conf.py
+++ telewater/conf.py
@@ -23,10 +23,14 @@ class Config(BaseModel):
 
 START = """正在工作中!"""
 
+""" 步骤4，改说明即可
+"""
 SETWM1 = """图片水印为『西安儒雅群』"""
 SETWM2 = """图片水印为『哈迪斯の后花园』"""
-SETWM3 = """全屏文字水印为『西安儒雅群t.me/xianruya』"""
-SETWM4 = """全屏文字水印为『@xahades http://t.me/xaHades』"""
+SETWM3 = """图片水印为『海纳百川』"""
+SETWM4 = """全屏文字水印为『西安儒雅群t.me/xianruya』"""
+SETWM5 = """全屏文字水印为『@xahades http://t.me/xaHades』"""
+SETWM6 = """全屏文字水印为『海纳百川 @hnbcshare』"""
 
 HELP = """
 使用方法很简单，只需要发送一张图片、视频、gif，机器人会自动打好水印并返回。
@@ -38,13 +42,17 @@ GET语法为： `/get` ➜  `/get key`
 
 """
 
+""" 步骤5，改说明即可
+"""
 COMMANDS = {
     "start": "尝试启动BOT，查看是否在工作中",
     "get": "获取当前配置",
     "watermark1" : "大水印：西安儒雅群",
     "watermark2" : "大水印：哈迪斯の后花园",
-    "watermark3" : "全屏文字：西安儒雅群t.me/xianruya",
-    "watermark4" : "全屏文字：@XaHades t.me/xaHades",
+    "watermark3" : "大水印：海纳百川",
+    "watermark4" : "全屏文字：西安儒雅群t.me/xianruya",
+    "watermark5" : "全屏文字：@XaHades t.me/xaHades",
+    "watermark6" : "全屏文字：海纳百川 @hnbcshare",
     "set": "设置当前配置",
     "help": "使用帮助",
 }
